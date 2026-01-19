4dfd31da9cf16ade88d65f49a9a29f1d6c30970d
Leonardo Hobeni <leonardohobeni@gmail.com>
2026-01-19 14:20:32 +0200

    Refactor: Update Post View Function to Update Post View Class

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import path
 from . import views
+from .views import NewPostView, UpdatePostView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_post/', views.add_post, name='add_post'),
-    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post')
+    path('add_post/', NewPostView.as_view(template_name='blogs/add_post.html'), name='add_post'),
+    path('edit_post/<int:post_id>/', UpdatePostView.as_view(template_name='blogs/edit_post.html'), name='edit_post')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse
+from django.views import View
 
 from .models import BlogPost
 from .forms import PostForm
@@ -14,43 +16,65 @@ def index(request):
     return render(request, 'blogs/index.html', context)
 
 
-@login_required
-def add_post(request):
-    """ This view will display form for new post """
+class NewPostView(View):
+    form_class = PostForm
+    template_name = 'blogs/add_post.html'
 
-    if request.method != 'POST':
-        # No data submitted; create blank form
-        form = PostForm()
-    else:
-        # POST data submitted; process data
-        form = PostForm(request.POST)
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+    
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             new_post = form.save(commit=False)
             new_post.owner = request.user
             new_post.save()
             return HttpResponseRedirect(reverse('index'))
+
+
+# @login_required
+# def edit_post(request, post_id):
+#     """ This view edits an existing post """
+
+#     post = get_object_or_404(BlogPost, pk=post_id, owner=request.user)
     
-    context = {'form':form}
-    return render(request, 'blogs/add_post.html', context)
-
-
-@login_required
-def edit_post(request, post_id):
-    """ This view edits an existing post """
-
-    post = get_object_or_404(BlogPost, pk=post_id)
-    if post.owner != request.user:
-        raise Http404
+#     if request.method != 'POST':
+#         # Initial request; populate fields
+#         form = PostForm(instance=post)
+#     else:
+#         # POST request; updating data with data in fields
+#         form = PostForm(instance=post, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('index'))
     
-    if request.method != 'POST':
-        # Initial request; populate fields
-        form = PostForm(instance=post)
-    else:
-        # POST request; updating data with data in fields
-        form = PostForm(instance=post, data=request.POST)
+#     context = {'post':post, 'form':form}
+#     return render(request, 'blogs/edit_post.html', context)
+
+class UpdatePostView(View):
+    form_class = PostForm
+    template_name = 'blogs/edit_post.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(BlogPost, pk=post_id)
+
+        if post.owner != request.user:
+            raise Http404
+        
+        form = self.form_class(instance=post)
+        return render(request, 'blogs/edit_post.html', {'form':form, 'post':post})
+    
+    def post(self, request, *args, **kwargs):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(BlogPost, pk=post_id)
+
+        form = self.form_class(instance=post, data=request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('index'))
-    
-    context = {'post':post, 'form':form}
-    return render(request, 'blogs/edit_post.html', context)
+        return render(request, 'blogs/edit_post.html', {'form':form, 'post':post})
